7f45b77be83a98c4d31c940f8541a20fb6905875
Litther <pablo.di@davinci.edu.ar>
2021-06-07 23:09:45 -0300

    Gura says AH

---
+++ MyA Sudoku Di Taranto - Giangrasso/Assets/Scripts/Board.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    private int[,] solvedGrid = new int[9, 9];
+    private string s;
+
+    private void Start()
+    {
+        InitGrid(ref solvedGrid);
+        DebugGrid(ref solvedGrid);
+    }
+
+    private void InitGrid(ref int[,] grid)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                grid[i, j] = (i * 3 + i / 3 + j) % 9 + 1;
+            }
+        }
+    }
+
+    private void DebugGrid(ref int[,] grid)
+    {
+        s = "";
+        int sep = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            s += "|";
+            for (int j = 0; j < 9; j++)
+            {
+                s += grid[i, j].ToString();
+
+                sep = j % 3;
+                if (sep == 2)
+                {
+                    s += "|";
+                }
+            }
+            s += "\n";
+        }
+        print(s);
+    }
+
+    private void ShuffleGrid(ref int[,] grid, int shuffleAmount)
+    {
+        for (int i = 0; i < shuffleAmount; i++)
+        {
+            int value1 = Random.Range(1, 10);
+            int value2 = Random.Range(1, 10);
+            MixTwoGridCells(ref grid, value1, value2);
+        }
+        DebugGrid(ref grid);
+    }
+
+    private void MixTwoGridCells(ref int[,] grid, int value1, int value2)
+    {
+        int x1 = 0;
+        int x2 = 0;
+
+        int y1 = 0;
+        int y2 = 0;
+
+        for (int i = 0; i < 9; i+=3)
+        {
+            for (int k = 0; k < 9; k+=3)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int l = 0; l < 3; l++)
+                    {
+                        if(grid[i+j, k+l] == value1)
+                        {
+                            x1 = i + j;
+                            y1 = k + l;
+                        }
+
+                        if (grid[i + j, k + l] == value2)
+                        {
+                            x2 = i + j;
+                            y2 = k + l;
+                        }
+                    }
+                }
+                grid[x1, y1] = value2;
+                grid[x2, y2] = value1;
+            }
+        }
+    }
+}
